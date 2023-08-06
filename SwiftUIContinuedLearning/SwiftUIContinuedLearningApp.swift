c2c4b00975c4a793d71e1bc8953ab4bb197d7f60
Softminilab <karepbq@gmail.com>
2023-08-06 20:57:27 +0800

    Added: init project

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by 0x2ab70001b1 on 2023/8/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
